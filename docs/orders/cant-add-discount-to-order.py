6d67e673d7ccd33bffc90e6811d1ac7ef4bb3e6a
jmpizzitola <53948316+jmpizzitola@users.noreply.github.com>
2025-02-18 22:23:22 -0600

    Create cant-add-discount-to-order.md

---
+++ docs/orders/cant-add-discount-to-order.py
@@ -0,0 +1,11 @@
+**Symptom**
+
+**Troubleshooting**
+The item or order meets the requirements of the discount. E.g., if a discount is $5 off of an order of $50 or more, the discount would not be available if the order total is $48. Requirements for a discount can include:
+		○ day of the week
+		○ time of the day
+		○ minimum order amount
+		○ specific items, item tags, or departments
+		○ service types
+		○ payment types
+	• The item doesn't already have that discount added. Although discounts can be configured to allow for multiple of the same discount to be applied to an item or order, the default on a new discount is limited to 1 per order. If the discount rule limits the number of times it can be used in a single order and that threshold has been met, then the discount will be greyed out.
